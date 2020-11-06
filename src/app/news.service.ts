c053858cc22730249a1596962de7b570aa982b2e
Rene Diana <rene.diana@netscrapers.com>
2020-11-06 19:21:19 +0100

    Vote binded to cookie

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -10,7 +10,7 @@ import { ReplaySubject } from "rxjs";
   providedIn: "root",
 })
 export class NewsService {
-  votes = new BehaviorSubject<{ [index: string]: number }>({});
+  votes = new BehaviorSubject<{ [index: string]: {myVote: number, total: number} }>({});
 
   readonly ROOT_URL = "http://localhost:3000/";
 
@@ -36,10 +36,10 @@ export class NewsService {
     );
   }
 
-  loadVotes(a: number): Observable<{ [index: string]: number }> {
+  loadVotes(a: number): Observable<{ [index: string]: {myVote: number, total: number} }> {
     return this.ids.pipe(
       mergeMap((i) =>
-        this.http.post<{ [index: string]: number }>(
+        this.http.post<{ [index: string]: {myVote: number, total: number} }>(
           this.ROOT_URL + `votes`,
           i.slice(a * 20, a * 20 + 20), {withCredentials: true}
         )
@@ -52,9 +52,9 @@ export class NewsService {
       map(([stories, votes]) =>
         stories.map<VotedStory>((story) => ({
           ...story,
-          vote: votes[story.id] ? votes[story.id] + story.score : story.score,
-          isUpVoted: votes[story.id] === 1,
-          isDownVoted: votes[story.id] === -1,
+          vote: votes[story.id] !== undefined ? votes[story.id].total + story.score : story.score,
+          isUpVoted: votes[story.id] !== undefined ? votes[story.id].myVote === 1 : false,
+          isDownVoted: votes[story.id] !== undefined ? votes[story.id].myVote === -1 : false,
         }))
       )
     );
@@ -63,8 +63,12 @@ export class NewsService {
   upVote(id: string) {
     const v = this.votes.value;
     const x = v[id];
-    v[id] = x === 1 ? undefined : 1;
-    const vote = x === 1 ? "resetvote" : "upvote";
+    const vote = x.myVote === 1 ? "resetvote" : "upvote";
+
+    v[id] = {
+      total: x.total + (x.myVote === undefined || x.myVote === -1 ? 1 : -1),
+      myVote: x.myVote === undefined ? 1 : undefined};
+
     this.votes.next(v);
     this.http.get(this.ROOT_URL + `item/${id}/${vote}`, {withCredentials: true}).subscribe();
   }
@@ -72,8 +76,12 @@ export class NewsService {
   downVote(id: string) {
     const v = this.votes.value;
     const x = v[id];
-    v[id] = x === -1 ? undefined : -1;
-    const vote = x === -1 ? "resetvote" : "downvote";
+    const vote = x.myVote === 1 ? "resetvote" : "downvote";
+
+    v[id] = {
+      total: x.total + (x.myVote === undefined || x.myVote === 1 ? -1 : 1),
+      myVote: x.myVote === undefined ? -1 : undefined};
+
     this.votes.next(v);
     this.http.get(this.ROOT_URL + `item/${id}/${vote}`, {withCredentials: true}).subscribe();
   }
